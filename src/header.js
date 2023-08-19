351c8d3856a26ea1670b82e297a6ff0784b0d921
Artem Voronyi <voronyi.artem@gmail.com>
2023-08-19 20:02:11 +0300

    fix: add new files Heade and Technogies

---
+++ src/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <div >
+            <nav className="nav">
+                <ul>
+                    <li><a href="#s">Home</a></li>
+                    <li><a href="#s">News Feed</a></li>
+                    <li><a href="#s">Message</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
